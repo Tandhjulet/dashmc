873787084195fc42bac617bb225656a2faa63787
Tandhjulet <madsbechmortensen@hotmail.dk>
2024-09-30 19:24:56 +0200

    Added dockerization

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ import createMDX from '@next/mdx'
 const nextConfig = {
 	pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 	transpilePackages: ['next-mdx-remote'],
+	output: "standalone",
 };
 
 const withMDX = createMDX({
