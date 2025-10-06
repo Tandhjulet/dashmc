9afa03a84869f187f27c06595479f05b15546d32
Mads Mortensen <mads@side-walk.dk>
2025-10-06 12:42:53 +0200

    patch: fix action mishap

--- src/app/(auth)/verify/page.tsx
+++ src/app/(auth)/verify/page.tsx
@@ -3,14 +3,8 @@
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 import VerifyUsername from "./VerifyUsername";
-import { baseUrl } from "@/app/sitemap";
 
-export default async function Verify(
-	props: {
-		searchParams?: Promise<{ [key: string]: string }>;
-	}
-) {
-	const searchParams = await props.searchParams;
+export default async function Verify() {
 	const session = await auth();
 	if (session && session.user?.email) {
 		const userExists = session.user.username;
@@ -19,20 +13,7 @@ export default async function Verify(
 			redirect("/");
 		} else {
 			return (
-				<VerifyUsername
-					callback={async (res) => {
-						"use server";
-						if (res.exists) {
-							// Ensures redirects can only happen to local pages
-							redirect(new URL(
-								decodeURIComponent((searchParams?.prevUrl) ?? "/"),
-								baseUrl,
-							).pathname);
-						} else {
-							redirect("/verify");
-						}
-					}
-					} />
+				<VerifyUsername />
 			)
 		}
 	} else {
